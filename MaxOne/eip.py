f96aeddc3c73145a45e7abdd08ea33c647d08913
221Bytes <catalanoalexandre@gmail.com>
2015-12-05 19:26:53 +0900

    add all files before cleaning

---
+++ MaxOne/eip.py
@@ -0,0 +1,65 @@
+from time import sleep
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+__author__ = 'necocityhunters'
+
+# cap = cv2.VideoCapture('testRecord.mp4')
+cap = cv2.VideoCapture(0)
+a = 0
+l_val = 0
+s_val = 0
+x_axis = []
+isTrue = True
+while isTrue:
+    ret, frame = cap.read()
+
+    y = frame.size
+
+    hls_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS)
+
+    bgr_image = cv2.cvtColor(hls_image, cv2.COLOR_HLS2BGR)
+
+    h, l, s = cv2.split(hls_image)
+
+    l += l_val
+
+    s += s_val
+
+    average = np.sum(l) / l.size
+    print(average)
+    x_axis.append(average)
+    hls_image[:, :, 1] = l
+    hls_image[:, :, 2] = s
+
+    key = cv2.waitKey(1)
+
+    img = np.concatenate((bgr_image, cv2.cvtColor(hls_image, cv2.COLOR_HLS2BGR)), 0)
+
+    cv2.imshow('test', img)
+
+    if key & 0xFF == ord('q'):
+        break
+    if key & 0xFF == ord('i'):
+        l_val += 1
+    elif key & 0xFF == ord('k'):
+        l_val -= 1
+    if key & 0xFF == ord('l'):
+        s_val += 1
+    elif key & 0xFF == ord('j'):
+        s_val -= 1
+    a += 1
+    if a == 210:
+        isTrue = False
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+plt.plot(x_axis, label=str('light'))
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=2, mode="expand", borderaxespad=0.)
+
+plt.show()
